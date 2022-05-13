4f9b37f9f037cc42b0b55babd55b54dec6f9a6e2
hongho092 <93205420+hongho092@users.noreply.github.com>
2022-05-13 10:25:35 +0700

    abc

--- ss08_validation/bt_ung_dung_nghe_nhac_update/src/main/java/com/example/dto/MusicDto.java
+++ ss08_validation/bt_ung_dung_nghe_nhac_update/src/main/java/com/example/dto/MusicDto.java
@@ -1,18 +1,25 @@
 package com.example.dto;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 public class MusicDto {
 
     private int id;
 
-    @NotBlank(message = "Can not be blank")
+    @NotBlank(message = "Can not be blank") /*không khoảng trắng được*/
+//    @NotEmpty(message = "cbjzhg") /*khoảng trắng được*/
+    @Size(max = 800, message = "Over 800")
+    @Pattern(regexp = "[A-Za-z]*", message = "Does not contain special characters")
     private String name;
 
     @NotBlank(message = "Can not be blank")
+    @Size(max = 800, message = "Over 300")
+    @Pattern(regexp = "[A-Za-z]*", message = "Does not contain special characters")
     private String singer;
 
     @NotBlank(message = "Can not be blank")
+    @Size(max = 800, message = "Over 1000")
+    @Pattern(regexp = "[A-Za-z,]*", message = "Does not contain special characters")
     private String kindOfMusic;
 
     @NotBlank(message = "Can not be blank")
